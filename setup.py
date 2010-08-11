73596f5f304e2458cbacc99e43019b2c1fc8797e
Rhett Garber <rhettg@gmail.com>
2010-08-10 17:43:11 -0700

    Now version 0.1.4

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name="testify",
-    version='0.1.3',
-	provides="testify",
+    version='0.1.4',
+    provides="testify",
     author="Yelp",
-    author_email="opensource@yelp.com",
-    url="http://github.com/Yelp/testify",
+    author_email="yelplabs@yelp.com",
+    url="http://github.com/Yelp/Testify",
     description='Testing framework',
     classifiers=[
         "Programming Language :: Python",
@@ -17,16 +17,20 @@ setup(
         "Development Status :: 4 - Beta",
     ],
     packages=["testify", "testify.utils"],
-	scripts=['bin/testify'],
-	long_description="""\
-Testify is a replacement for Python's unittest module.  It is modeled after unittest, and tests written for unittest will run under testify with a minimum of adjustments, but it has features above and beyond unittest:
+    scripts=['bin/testify'],
+    long_description="""Testify - A Testing Framework
+
+Testify is a replacement for Python's unittest module and nose. It is modeled after unittest, and tests written for unittest
+will run under testify with a minimum of adjustments, but it has features above and beyond unittest:
 
   - class-level setup and teardown fixture methods which are run once each for an entire set of test methods.
   - a decorator-based approach for fixture methods, eliminating the need for super() calls.
-  - More pythonic, less java-unittest
-  - enhanced test discovery - testify can drill down into packages to find test cases.
-  - support for collecting and running tests by 'tagging' modules, classes or methods.
-  - A decorator-based approach to temporarily disabling certain tests, which strongly encourages documentation and eventual fixing of bugs.
+  - More pythonic, less java
+  - enhanced test discovery - testify can drill down into packages to find test cases (similiar to nose).
+  - support for collecting and running tests by collecting modules, classes or methods into test suites.
   - Pretty test runner output (color!)
-	"""
+  - Support for splitting up tests into buckets to support multi-processing
+  - Extensible plugin system for adding additional functionality around reporting
+  - Comes complete with other handy testing utilities: Mocking (turtle), code coverage integration and profiling.
+"""
 )
