d3bab87f3e81f57ca2acd45b952aa655ecfaa131
Evan Krall <krall@yelp.com>
2012-03-09 11:52:07 -0800

    Make AsyncQueue more thread-safe. Shut down once we think all builders have been told to finish.

--- testify/test_runner_server.py
+++ testify/test_runner_server.py
@@ -22,10 +22,12 @@ import logging
 import Queue
 import time
 import itertools
+import threading
 
 
 class AsyncQueue(object):
     def __init__(self):
+        self.lock = threading.Lock()
         self.data_queue = Queue.PriorityQueue()
         self.callback_queue = Queue.PriorityQueue()
         self.finalized = False
@@ -33,22 +35,24 @@ class AsyncQueue(object):
     def get(self, c_priority, callback):
         """If the queue is not empty, call callback immediately with the next item. Otherwise, put callback in a callback priority queue, to be called when data is put().
         If finalize() is called before data arrives for callback, callback(None, None) is called."""
-        if self.finalized:
-            callback(None, None)
-            return
-        try:
-            d_priority, data = self.data_queue.get_nowait()
-            callback(d_priority, data)
-        except Queue.Empty:
-            self.callback_queue.put((c_priority, callback,))
+        with self.lock:
+            if self.finalized:
+                callback(None, None)
+                return
+            try:
+                d_priority, data = self.data_queue.get_nowait()
+                callback(d_priority, data)
+            except Queue.Empty:
+                self.callback_queue.put((c_priority, callback,))
 
     def put(self, d_priority, data):
-        """If a get callback is waiting, call it immediately with this data. Otherwise, put data in a priority queue, to be retrieved at a future date."""
-        try:
-            c_priority, callback = self.callback_queue.get_nowait()
-            callback(d_priority, data)
-        except Queue.Empty:
-            self.data_queue.put((d_priority, data,))
+        with self.lock:
+            """If a get callback is waiting, call it immediately with this data. Otherwise, put data in a priority queue, to be retrieved at a future date."""
+            try:
+                c_priority, callback = self.callback_queue.get_nowait()
+                callback(d_priority, data)
+            except Queue.Empty:
+                self.data_queue.put((d_priority, data,))
 
     def empty(self):
         return self.data_queue.empty()
@@ -58,13 +62,14 @@ class AsyncQueue(object):
 
     def finalize(self):
         """Call all queued callbacks with None, and make sure any future calls to get() immediately call their callback with None."""
-        self.finalized = True
-        try:
-            while True:
-                _, callback = self.callback_queue.get_nowait()
-                callback(None, None)
-        except Queue.Empty:
-            pass
+        with self.lock:
+            self.finalized = True
+            try:
+                while True:
+                    _, callback = self.callback_queue.get_nowait()
+                    callback(None, None)
+            except Queue.Empty:
+                pass
 
 class TestRunnerServer(TestRunner):
     def __init__(self, *args, **kwargs):
@@ -79,6 +84,8 @@ class TestRunnerServer(TestRunner):
         self.timeout_rerun_methods = set() # Set of (class_path, method) who were sent to a client but results never came.
         self.previous_run_results = {} # Keyed on (class_path, method), values are result dictionaries.
         self.runners = set() # The set of runner_ids who have asked for tests.
+        self.runners_outstanding = set() # The set of runners who have posted results but haven't asked for the next test yet.
+        self.shutting_down = False # Whether shutdown() has been called.
 
         super(TestRunnerServer, self).__init__(*args, **kwargs)
 
@@ -111,25 +118,45 @@ class TestRunnerServer(TestRunner):
             @tornado.web.asynchronous
             def get(handler):
                 runner_id = handler.get_argument('runner')
+
+                if self.shutting_down:
+                    self.runners_outstanding.discard(runner_id)
+                    return handler.finish(json.dumps({
+                        'finished': True,
+                    }))
+
                 if self.revision and self.revision != handler.get_argument('revision'):
                     return handler.send_error(409, reason="Incorrect revision %s -- server is running revision %s" % (handler.get_argument('revision'), self.revision))
 
                 def callback(test_dict):
+                    self.runners_outstanding.discard(runner_id)
                     handler.finish(json.dumps({
                         'class': test_dict['class_path'],
                         'methods': test_dict['methods'],
                         'finished': False,
                     }))
+
                 def empty_callback():
+                    self.runners_outstanding.discard(runner_id)
                     handler.finish(json.dumps({
                         'finished': True,
                     }))
 
                 self.get_next_test(runner_id, callback, empty_callback)
 
+            def finish(handler, *args, **kwargs):
+                super(TestsHandler, handler).finish(*args, **kwargs)
+                tornado.ioloop.IOLoop.instance().add_callback(handler.after_finish)
+
+            def after_finish(handler):
+                if self.shutting_down and not self.runners_outstanding:
+                    iol = tornado.ioloop.IOLoop.instance()
+                    iol.add_callback(iol.stop)
+
         class ResultsHandler(tornado.web.RequestHandler):
             def post(handler):
                 runner_id = handler.get_argument('runner')
+                self.runners_outstanding.add(runner_id)
                 result = json.loads(handler.request.body)
 
                 class_path = '%s %s' % (result['method']['module'], result['method']['class'])
@@ -326,8 +353,13 @@ class TestRunnerServer(TestRunner):
         self.shutdown()
 
     def shutdown(self):
-        # Can't immediately call stop, otherwise the runner currently POSTing its results will get a Connection Refused when it tries to ask for the next test.
+        self.shutting_down = True
         self.test_queue.finalize()
         iol = tornado.ioloop.IOLoop.instance()
-        iol.add_timeout(time.time()+1, iol.stop)
+        # Can't immediately call stop, otherwise the runner currently POSTing its results will get a Connection Refused when it tries to ask for the next test.
 
+        if self.runners_outstanding:
+            # Stop in 5 seconds if all the runners_outstanding don't come back by then.
+            iol.add_timeout(time.time()+5, iol.stop)
+        else:
+            iol.stop()
