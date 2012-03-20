9d0a665c331637887240ffb67f3c459746bd4cdb
Evan Krall <krall@yelp.com>
2012-03-19 18:52:39 -0700

    Add setup_teardown and class_setup_teardown fixtures.

--- test/plugins/http_reporter_test.py
+++ test/plugins/http_reporter_test.py
@@ -1,17 +1,22 @@
-import contextlib
 import json
 import threading
 import tornado.ioloop
 import tornado.httpserver
 import tornado.web
 
-from testify import TestCase, assert_equal
+from testify import assert_equal, setup_teardown, TestCase
 from testify.test_runner import TestRunner
 from testify.plugins.http_reporter import HTTPReporter
 
+class DummyTestCase(TestCase):
+	__test__ = False
+	def test(self):
+		pass
+
+
 class HTTPReporterTestCase(TestCase):
-	@contextlib.contextmanager
-	def fake_server(self):
+	@setup_teardown
+	def make_fake_server(self):
 		self.results_reported = []
 
 		class ResultsHandler(tornado.web.RequestHandler):
@@ -29,24 +34,21 @@ class HTTPReporterTestCase(TestCase):
 		thread = threading.Thread(target=iol.start)
 		thread.daemon = True # If for some reason, this thread gets blocked, don't prevent quitting.
 		thread.start()
-		try:
-			yield "localhost:%d" % portnum
-		finally:
-			iol.stop()
-			thread.join()
+
+		self.connect_addr = "localhost:%d" % portnum
+
+		yield
+
+		iol.stop()
+		thread.join()
 
 	def test_http_reporter_reports(self):
 		"""A simple test to make sure the HTTPReporter actually reports things."""
 
-		class DummyTestCase(TestCase):
-			def test(self):
-				pass
+		runner = TestRunner(DummyTestCase, test_reporters=[HTTPReporter(None, self.connect_addr, 'runner1')])
+		runner.run()
 
-		with self.fake_server() as connect_addr:
-			runner = TestRunner(DummyTestCase, test_reporters=[HTTPReporter(None, connect_addr, 'runner1')])
-			runner.run()
-
-			(only_result,) = self.results_reported
-			assert_equal(only_result['runner_id'], 'runner1')
-			assert_equal(only_result['method']['class'], 'DummyTestCase')
-			assert_equal(only_result['method']['name'], 'test')
+		(only_result,) = self.results_reported
+		assert_equal(only_result['runner_id'], 'runner1')
+		assert_equal(only_result['method']['class'], 'DummyTestCase')
+		assert_equal(only_result['method']['name'], 'test')
