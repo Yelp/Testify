8d92a7b85b168af9112fc51500ed423d8ac04441
Evan Krall <krall@yelp.com>
2012-05-15 14:56:56 -0700

    Docstrings for assertion functions.

--- testify/assertions.py
+++ testify/assertions.py
@@ -42,6 +42,7 @@ def _diff_message(lhs, rhs):
     return 'Diff:\nl: %s\nr: %s' % stringdiffer.highlight(lhs, rhs)
 
 def assert_equal(lval, rval, message=None):
+    """Assert that lval and rval are equal."""
     if message:
         assert lval == rval, message
     else:
@@ -52,44 +53,53 @@ def assert_equal(lval, rval, message=None):
 assert_equals = assert_equal
 
 def assert_almost_equal(lval, rval, digits, message=None):
+    """Assert that lval and rval, when rounded to the specified number of digits, are the same."""
     real_message = message or "%r !~= %r" % (lval, rval)
     assert round(lval, digits) == round(rval, digits), real_message
 
 def assert_within_tolerance(lval, rval, tolerance, message=None):
+    """Assert that the difference between the two values, as a fraction of the left value, is smaller than the tolerance specified.
+    That is, abs(float(lval) - float(rval)) / float(lval) < tolerance"""
     real_message = message or "%r !~= %r" % (lval, rval)
     assert abs(float(lval) - float(rval)) / float(lval) < tolerance, real_message
 
 def assert_not_equal(lval, rval, message=None):
+    """Assert that lval and rval are unequal to each other."""
     if message:
         assert lval != rval, message
     else:
         assert lval != rval, 'assertion failed: %s != %s' % (lval, rval)
 
 def assert_lt(lval, rval, message=None):
+    """Assert that lval is less than rval."""
     if message:
         assert lval < rval, message
     else:
         assert lval < rval, 'assertion failed: %s < %s' % (lval, rval)
 
 def assert_lte(lval, rval, message=None):
+    """Assert that lval is less than or equal to rval"""
     if message:
         assert lval <= rval, message
     else:
         assert lval <= rval, 'assertion failed: %s lte %s' % (lval, rval)
 
 def assert_gt(lval, rval, message=None):
+    """Assert that lval is greater than rval."""
     if message:
         assert lval > rval, message
     else:
         assert lval > rval, 'assertion failed: %s > %s' % (lval, rval)
 
 def assert_gte(lval, rval, message=None):
+    """Assert that lval is greater than or equal to rval"""
     if message:
         assert lval >= rval, message
     else:
         assert lval >= rval, 'assertion failed: %s >= %s' % (lval, rval)
 
 def assert_in_range(val, start, end, message=None, inclusive=False):
+    """Assert that val is greater than start and less than end. If inclusive is true, val may be equal to start or end."""
     if inclusive:
         real_message = message or "! %s <= %r <= %r" % (start, val, end)
         assert start <= val <= end, real_message
@@ -98,16 +108,20 @@ def assert_in_range(val, start, end, message=None, inclusive=False):
         assert start < val < end, real_message
 
 def assert_in(item, sequence):
+    """Assert that the item is in the sequence."""
     assert item in sequence, "assertion failed: expected %r in %r" % (item, sequence)
 
 def assert_not_in(item, sequence):
+    """Assert that the item is not in the sequence."""
     assert item not in sequence, "assertion failed: expected %r not in %r" % (item, sequence)
 
 def assert_starts_with(val, prefix):
+    """Assert that val.startswith(prefix)."""
     msg = "%(val)r does not start with %(prefix)r" % locals()
     assert val.startswith(prefix), msg
 
 def assert_not_reached(message=None):
+    """Raise an AssertionError with a message."""
     if message:
         assert False, message
     else:
