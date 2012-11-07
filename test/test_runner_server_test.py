4023d46ac4f5a5e99cef7c40e5a46d4bd35129bd
Tyler Roscoe <troscoe@yelp.com>
2012-11-06 16:59:46 -0800

    It's ugly but it's a test that validates failure_limit behavior.

--- test/test_runner_server_test.py
+++ test/test_runner_server_test.py
@@ -299,4 +299,64 @@ class TestRunnerServerExceptionInClassFixtureTestCase(TestRunnerServerBaseTestCa
         assert_equal(self.server.test_queue.empty(), True)
 
 
+class TestRunnerServerFailureLimitTestCase(TestRunnerServerBaseTestCase):
+    def build_test_case(self):
+        class FailureLimitTestCase(test_case.TestCase):
+            def __init__(self, *args, **kwargs):
+                super(FailureLimitTestCase, self).__init__(*args, **kwargs)
+                self.failure_limit = 2
+
+            def test1(self):
+                print "in test1. limit: %s. count: %s." % (self.failure_limit, self.failure_count)
+                assert False, "I am the first failure. failure_limit is %s" % self.failure_limit
+
+            def test2(self):
+                print "in test2. limit: %s. count: %s." % (self.failure_limit, self.failure_count)
+                assert False, "I am the second (and last) failure. failure_limit is %s" % self.failure_limit
+
+            def test3(self):
+                print "in test3. limit: %s. count: %s." % (self.failure_limit, self.failure_count)
+                assert False, "This test should not run because failure_count (%s) >= failure_limit (%s)." % (self.failure_count, self.failure_limit)
+
+        self.dummy_test_case = FailureLimitTestCase
+
+    @setup
+    def setup_server(self):
+        """Override parent's setup_server so we can pass in a test_reporter."""
+        self.test_reporter = turtle.Turtle()
+        test_reporters = [self.test_reporter]
+        self.start_server(test_reporters=test_reporters)
+
+    def run_test(self, runner_id, should_pass=True):
+        test_instance = self.dummy_test_case()
+        for event in [
+            test_case.TestCase.EVENT_ON_COMPLETE_TEST_METHOD,
+        ]:
+            test_instance.register_callback(
+                event,
+                lambda result: self.test_reporter.on_complete_test_method(result)
+            )
+
+        test_instance.run()
+    def test(self):
+        get_test(self.server, 'runner')
+        self.check_in_class = turtle.Turtle()
+        self.run_test('runner')
+
+        # Due to failure_limit, only the first two tests should run.
+        expected_methods = set(['test1', 'test2'])
+        seen_methods = set()
+
+        test_method_complete_calls = self.test_reporter.on_complete_test_method.calls
+        for call in test_method_complete_calls:
+            args = call[0]
+            first_arg = args[0]
+            first_method_name = first_arg['method']['name']
+            seen_methods.add(first_method_name)
+        assert_equal(expected_methods.symmetric_difference(seen_methods), set())
+
+        # Verify the failed class_teardown method is not re-queued for running.
+        assert_equal(self.server.test_queue.empty(), True)
+
+
 # vim: set ts=4 sts=4 sw=4 et:
