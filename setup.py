9707b2b45a588b926af57cc26eb94a36e335baa4
Evan Krall <krall@yelp.com>
2012-12-07 15:58:39 -0800

    Capitalize mock, remove spurious comma

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "Intended Audience :: Developers",
         "Development Status :: 4 - Beta",
     ],
-    install_requires=['mock,', 'PyYaml', 'SQLAlchemy', 'Tornado'],
+    install_requires=['Mock', 'PyYaml', 'SQLAlchemy', 'Tornado'],
     packages=["testify", "testify.contrib", "testify.utils", "testify.plugins"],
     scripts=['bin/testify'],
     long_description="""Testify - A Testing Framework
