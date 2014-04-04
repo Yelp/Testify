bfb2a178705c4f69b07989a33089cd68379f8aa8
Buck Golemon <workitharder@gmail.com>
2014-04-04 13:56:57 -0700

    v0.5.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="testify",
-    version="0.5.3",
+    version="0.5.4",
     provides=["testify"],
     author="Yelp",
     author_email="yelplabs@yelp.com",
