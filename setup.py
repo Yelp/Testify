4cbb4771d39172d7516da7dbba1f9b0aa6c0e09b
milki <milki@yelp.com>
2014-07-21 08:53:26 -0700

    Bump to testify 0.5.5
      * Fix failure reporting with only one slave in client/server model
        (asottile, #240, #264, #274)
      * Dynamically annotate unittests using catbox data (astephen2, #241)
      * Fix regressions for windows (jareyes409, #245)
      * Enable retries in sql reporter for SQL connection (bukzor, #249)
      * Fix (run, test) ordering check in test_runner_server_test (asottile, #255)
      * Fix flakey tests that depended on order (asottile, #257)
      * Fix timing out test (asottile, #262)
      * Standardize retry options (bukzor, #266)
      * Fix flakey test with sql reporter (bukzor, #267)
      * Use flake8 (astephen2, #271)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="testify",
-    version="0.5.4",
+    version="0.5.5",
     provides=["testify"],
     author="Yelp",
     author_email="yelplabs@yelp.com",
