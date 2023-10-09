fe1b8d02a099f21058e050a4d9471f874d4e44b3
Ben Bariteau <bariteau@yelp.com>
2023-10-09 16:51:14 -0700

    release v0.11.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="testify",
-    version="0.11.2",
+    version="0.11.3",
     provides=["testify"],
     author="Yelp",
     author_email="yelplabs@yelp.com",
